d8866d7bd70008df554566b8d2708e1931aad313
bluebill1049 <bluebill1049@hotmail.com>
2019-11-22 11:09:58 +1100

    improve types

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
 
+export type ValidateResult = string | boolean | undefined;
+
+export type Validate = (data: any) => ValidateResult;
+
+type ValidationOptionObject<Value> = Value | { value: Value; message: string };
+
+type ValidationOptions = Partial<{
+  required: boolean | string;
+  min: ValidationOptionObject<number | string>;
+  max: ValidationOptionObject<number | string>;
+  maxLength: ValidationOptionObject<number | string>;
+  minLength: ValidationOptionObject<number | string>;
+  pattern: ValidationOptionObject<RegExp>;
+  validate:
+    | Validate
+    | Record<string, Validate>
+    | { value: Validate | Record<string, Validate>; message: string };
+}>;
+
 type Props = {
   setValue: (name: string, value: any, trigger?: boolean) => void;
-  register: (ref: any, rules: any) => void;
+  register: (ref: any, rules: ValidationOptions) => void;
   name: string;
   component: any;
   rules?: any;
